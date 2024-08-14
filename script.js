608cf1ce5bdbd1aa248fda6a17a8d770fedaf37c
rodelul <98654686+rodelul@users.noreply.github.com>
2024-08-14 18:07:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Get the button and the div elements
-    var button = document.querySelector('.button-open');
-    var shareButton = document.querySelector('.share-button');
+    let button = document.querySelector('.button-open');
+    let shareButton = document.querySelector('.share-button');
 
     // Add a click event listener to the button
     button.addEventListener('click', function () {
         // Toggle the display property of the shareButton div
         if (shareButton.style.display === 'none' || shareButton.style.display === '') {
-            shareButton.style.display = 'block';
+            shareButton.style.display = 'flex';
         } else {
             shareButton.style.display = 'none';
         }
